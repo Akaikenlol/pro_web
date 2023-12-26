0b8acefd4f11b44974690daec92c46c47c6e8ff4
Akaikenlol <keithken972@gmail.com>
2023-12-26 17:26:46 +0630

    feat: Search Input

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import { Input } from "./ui/input";
 
 const SearchForm = () => {
-	return <div>SearchForm</div>;
+	const [search, setSearch] = useState("");
+	return (
+		<form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
+			<label className="flex-center relative w-full max-w-3xl">
+				<Image
+					src={"/magnifying-glass.svg"}
+					alt="Search Icon"
+					width={32}
+					height={32}
+					className="absolute left-8"
+				/>
+				<Input
+					type="text"
+					placeholder="Search"
+					className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+			</label>
+		</form>
+	);
 };
 
 export default SearchForm;
