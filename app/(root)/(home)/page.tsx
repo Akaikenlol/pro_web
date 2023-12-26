a5e40d4d19b852816d6fcf625c53d6c454790f96
Akaikenlol <keithken972@gmail.com>
2023-12-26 15:48:42 +0630

    feat: Footer

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Page = () => {
-	return <div>Home Page</div>;
+	return (
+		<div>
+			<h1>Home Page</h1>
+			<h1>Home Page</h1>
+			<h1>Home Page</h1>
+			<h1>Home Page</h1>
+			<h1>Home Page</h1>
+		</div>
+	);
 };
 
 export default Page;
